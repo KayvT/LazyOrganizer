ad2874ba3dad226ac24566656a40101942e54bcf
Khaled <khaledyassen09@gmail.com>
2021-09-21 18:23:48 +0300

    reset layout, add screens

--- organizer.py
+++ organizer.py
@@ -5,39 +5,38 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 kivy.require('2.0.0')
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
-class Organizer(App):
+#Screens
+class PathScreen(Screen):
+    pass
 
+class WelcomeScreen(Screen):
+    pass
+
+
+#Design Files
+
+
+class Organizer(ScreenManager):
+    pass
+kv = Builder.load_file('new_window.kv')
+
+#Manager
+class MyApp(App):
     def build(self):
-        # Build Components
-        self.window = GridLayout()
-        self.window.cols = 1
+        return kv
 
 
-
-        ## Widgets
-        self.initalGreetingLabel = Label(text="Hi there! What should I call you?")
-        self.user_name = TextInput(multiline=False)
-        self.enter_button = Button(text="Take me in!")
-
-        self.enter_button.bind(on_press=self.proceed)
-
-        self.window.add_widget(self.initalGreetingLabel)
-        self.window.add_widget(Image(source="./assets/logo.png"))
-        self.window.add_widget(self.user_name)
-        self.window.add_widget(self.enter_button)
-        
-
-        return self.window
-
-    def proceed(self, instance):
-        self.initalGreetingLabel.text = "Hi, " + self.user_name.text + "!"
 
 if __name__ == '__main__':
-    Organizer().run()
+
+    MyApp().run()
     # print(kivy.__version__) #You should use version 2.0.0
 
 
